c5e3d81bac91dd5e057eae4dc732623d410f9a1a
IamRojal <140803638+IamRojal@users.noreply.github.com>
2023-08-06 12:37:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-// import { list } from "./src/country.js";
+// top of file
+import { API } from '@aws-amplify/api'
+import config from './src/aws-exports.js'
+import { listCountries } from './src/graphql/queries.js'
+import { createCountry } from './src/graphql/mutations.js'
 
-// list();
-import { API } from '@aws-amplify/api';
-import config from './country/src/aws-exports.js';
-import { listCountries } from './country/src/graphql/queries.js';
-API.configure(config);
-exports.handler = async function (event) {
-    console.log('hai');
-    let response = await API.graphql({
-        query: listCountries,
-        variables: {
-            // Optional: If you want to add any filters or pagination variables, you can define them here.
-            // Example: filter: { id: { eq: 'someId' } }, limit: 10, nextToken: null
-        },
-    })
-    return { message: 'success' };
+// after your imports
+API.configure(config)
+
+export const handler = async(event) => {
+     try {
+   const requestBody = event?.body?JSON.parse(event.body):'nodata';
+   
+   if(requestBody.type=='add'){
+       const variables = {
+      input: {
+        ...requestBody.data
+      },
+    };
+        const response = await API.graphql({query: createCountry,variables})
+         console.log(response);
+        return {status:'200',data:'successssss',requestBody:requestBody};
+   }
+
+    // return {status:'200eee',data:{event:event,abc:abc}};
+   
+       const response = await API.graphql({      query: listCountries,   })
+         console.log(response);
+        return {status:'200',data:response?.data?.listCountries?.items||[],requestBody:requestBody};
+    }
+    catch (e) {
+        return {status:'500',error:e,data:[]};
+    }
 };
+
+// later down in your code
+// async function list() {
+//    const response = await API.graphql({
+//       query: listCountries,
+//    })
+//    console.log(response)
+// }
+// list()
